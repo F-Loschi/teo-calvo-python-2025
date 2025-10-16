ebd9c685b16c454bdb9632e55930f978d7c73bfa
Felipera <f.s.loschi@gmail.com>
2025-10-16 17:48:26 -0300

    Feat: Entendendo Series

--- Pandas/Dia01/02_series.py
+++ Pandas/Dia01/02_series.py
@@ -1,4 +1,21 @@
 #%%
 import pandas as pd
-
 #%%
+#Series é melhor que listas por possuir mais funcionalidades
+#Mas diferente de listas, Series são unidimensionais
+#Identifica o tipo de dado automaticamente
+#Exemplo 1 - Criando uma Series a partir de uma lista
+idades = [39, 30, 27, 18, 15, 12, 10]
+series_idades = pd.Series(idades)
+series_idades
+# %%
+media = series_idades.mean()
+media
+variancia = series_idades.var()
+variancia
+desvio_padrao = series_idades.std()
+desvio_padrao
+# %%
+summary = series_idades.describe()
+summary
+# %%
